205db37b2540c99ece6bf65ac84cc740abdbf16f
diodon <ekleins@gmail.com>
2020-10-08 10:03:31 +1100

    add filedescription function

--- Code/R/getGriddedTS.R
+++ Code/R/getGriddedTS.R
@@ -1,6 +1,5 @@
 ## Get time series from gridded
 library(ncdf4)
-library(lubridate)
 
 getGriddedFileName = function(site){
   urlGeoServer = paste0("http://geoserver-123.aodn.org.au/geoserver/ows?typeName=moorings_all_map&SERVICE=WFS&REQUEST=GetFeature&VERSION=1.0.0&outputFormat=csv&CQL_FILTER=(realtime='FALSE')and(site_code='", site, "')")
@@ -9,7 +8,20 @@ getGriddedFileName = function(site){
   return(fileName)
 }
 
-getGriddedTemp = function(site, depth, dateStart="1970-01-01", dateEnd = Sys.Date()){
+
+fileDescription = function(nc){
+  sourceFile = ncatt_get(nc,0,"source_file")$value
+  print(paste0("SOURCE FILE: ", sourceFile))
+  print(paste0("SITE: ", ncatt_get(nc,0,"site_code")$value))
+  print(paste0("FACILITY: ", strsplit(sourceFile, "/")[[1]][[3]]))
+  latMean = mean(ncatt_get(nc,0,"geospatial_lat_min")$value, ncatt_get(nc,0,"geospatial_lat_max")$value)
+  lonMean = mean(ncatt_get(nc,0,"geospatial_lon_min")$value, ncatt_get(nc,0,"geospatial_lon_max")$value)
+  print(paste0("LOCATION: ", latMean, " / ", lonMean))
+  print(paste0("TIME COVERAGE: ", ncatt_get(nc,0,"time_coverage_start")$value, " - ", ncatt_get(nc,0,"time_coverage_end")$value))
+  print(paste0("AVAILABLE DEPTHS: ", paste0(ncvar_get(nc, "DEPTH"), collapse=", ")))
+}
+
+getGriddedTemp = function(site, depth, dateStart="1970-01-01", dateEnd = Sys.Date(), saveFile=FALSE){
   #site="GBRPPS"
   #depth = 20
   
@@ -21,6 +33,7 @@ getGriddedTemp = function(site, depth, dateStart="1970-01-01", dateEnd = Sys.Dat
   
   if (!depth %in% depthList){
     print(paste0("ERROR: requested DEPTH (", depth, "m) is not available"))
+    fileDescription(nc)
   }else {
     ## get depth index
     depthIndex = which(depthList==depth)
@@ -33,11 +46,14 @@ getGriddedTemp = function(site, depth, dateStart="1970-01-01", dateEnd = Sys.Dat
     ## filter dates
     df = df[df$TIME>=as.POSIXct(dateStart) & df$TIME<=as.POSIXct(dateEnd),]
   
-    @## wrtie file
-    outFileName = paste0(paste(site,"TEMP", paste0(depth,"m"), 
-                        paste0(format(min(TS.time), "%Y%m%d"),"-", format(max(TS.time), "%Y%m%d")),
-                        sep = "_"),".csv")
-    write.csv(file=outFileName, df, row.names = FALSE)
+    ## write file
+    outFileName = NA
+    if (saveFile){
+      outFileName = paste0(paste(site,"TEMP", paste0(depth,"m"), 
+                          paste0(format(min(TS.time), "%Y%m%d"),"-", format(max(TS.time), "%Y%m%d")),
+                          sep = "_"),".csv")
+      write.csv(file=outFileName, df, row.names = FALSE)
+    }
     
     return(list(DATA=df, outputFileName=outFileName))
   }
